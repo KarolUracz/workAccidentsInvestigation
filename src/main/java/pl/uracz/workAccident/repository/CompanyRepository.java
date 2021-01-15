023fe1b1f4b102f59daef338510d661e342cb0cf
Karol Uracz <karol_ur@interia.pl>
2021-01-15 13:55:36 +0100

    empty company repostitory and service

---
+++ src/main/java/pl/uracz/workAccident/repository/CompanyRepository.java
@@ -0,0 +1,9 @@
+package pl.uracz.workAccident.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.uracz.workAccident.entity.Company;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
---
+++ src/main/java/pl/uracz/workAccident/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,14 @@
+package pl.uracz.workAccident.service.impl;
+
+import org.springframework.stereotype.Service;
+import pl.uracz.workAccident.repository.CompanyRepository;
+import pl.uracz.workAccident.service.CompanyService;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+    private CompanyRepository companyRepository;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+}
