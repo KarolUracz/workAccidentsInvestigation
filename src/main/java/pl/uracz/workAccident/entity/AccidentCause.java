95ab933b5742b8e83747bb9d236614dedbc29550
Karol Uracz <karol_ur@interia.pl>
2021-01-18 17:43:06 +0100

    added lombok annotations to entities

--- src/main/java/pl/uracz/workAccident/entity/AccidentCause.java
+++ src/main/java/pl/uracz/workAccident/entity/AccidentCause.java
@@ -1,30 +1,17 @@
 package pl.uracz.workAccident.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class AccidentCause {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String accidentCause;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAccidentCause() {
-        return accidentCause;
-    }
-
-    public void setAccidentCause(String accidentCause) {
-        this.accidentCause = accidentCause;
-    }
 }
--- src/main/java/pl/uracz/workAccident/entity/VictimAddress.java
+++ src/main/java/pl/uracz/workAccident/entity/VictimAddress.java
@@ -1,11 +1,14 @@
 package pl.uracz.workAccident.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class VictimAddress {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,68 +20,4 @@ public class VictimAddress {
     private Integer apartmentNumber;
     private String peselNumber;
     private String identificationDocumentNumber;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPostCode() {
-        return postCode;
-    }
-
-    public void setPostCode(String postCode) {
-        this.postCode = postCode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getHouseNumber() {
-        return houseNumber;
-    }
-
-    public void setHouseNumber(String houseNumber) {
-        this.houseNumber = houseNumber;
-    }
-
-    public Integer getApartmentNumber() {
-        return apartmentNumber;
-    }
-
-    public void setApartmentNumber(Integer apartmentNumber) {
-        this.apartmentNumber = apartmentNumber;
-    }
-
-    public String getPeselNumber() {
-        return peselNumber;
-    }
-
-    public void setPeselNumber(String peselNumber) {
-        this.peselNumber = peselNumber;
-    }
-
-    public String getIdentificationDocumentNumber() {
-        return identificationDocumentNumber;
-    }
-
-    public void setIdentificationDocumentNumber(String identificationDocumentNumber) {
-        this.identificationDocumentNumber = identificationDocumentNumber;
-    }
 }
